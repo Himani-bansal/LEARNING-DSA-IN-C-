d4b97f97aa05155dbc6b759494cc847e40a80248
Himani-bansal <himanibansal137@gmail.com>
2024-03-14 22:00:11 +0530

    13-march complete

---
+++ PRACTISE QUESTION/13-march/Predict.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int j;
+    while (j <= 10)
+    {
+        cout << j << endl;
+        j = j + 1;
+    }
+    return 0;
+}
+
+// It is not displaying anything as we had not given any value to j to start with so loop will not run.
