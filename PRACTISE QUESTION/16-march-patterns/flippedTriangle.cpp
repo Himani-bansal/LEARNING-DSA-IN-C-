b43fcc547a4f519ed5c0058d34f39bb860945491
Himani-bansal <himanibansal137@gmail.com>
2024-03-16 17:56:37 +0530

    patterns 16-march

---
+++ PRACTISE QUESTION/16-march-patterns/flippedTriangle.cpp
@@ -0,0 +1,30 @@
+/*
+printing
+1 2 3 4
+ 1 2 3
+  1 2
+   1
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of rows" << endl;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
