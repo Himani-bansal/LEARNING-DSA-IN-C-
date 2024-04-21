4227b72e5cf363946d0799e0aa2752504f5ea037
Himani-bansal <himanibansal137@gmail.com>
2024-04-21 17:32:09 +0530

    addMatrix

--- PRACTISE QUESTION/28-march-2dArray/add2Matrices.cpp
+++ PRACTISE QUESTION/28-march-2dArray/add2Matrices.cpp
@@ -4,35 +4,35 @@ using namespace std;
 int main()
 {
     // 1st Matrix
-    int row,col;
-    cout<<"Enter the number of rows and columns of 1st Matrix: "<<endl;
-    cin>>row>>col;
+    int row, col;
+    cout << "Enter the number of rows and columns of 1st Matrix: " << endl;
+    cin >> row >> col;
     int arr[row][col];
-    cout<<"Enter the elements of the 1st Matrix: "<<endl;
-    for(int i=0;i<row;i++)
+    cout << "Enter the elements of the 1st Matrix: " << endl;
+    for (int i = 0; i < row; i++)
     {
-        for(int j=0; j<col;j++)
+        for (int j = 0; j < col; j++)
         {
-            cin>>arr[i][j];
+            cin >> arr[i][j];
         }
     }
     // 2nd Matrix
     int arr2[row][col];
-    cout<<"Enter the elements of the 2nd Matrix: "<<endl;
-    for(int i=0;i<row;i++)
+    cout << "Enter the elements of the 2nd Matrix: " << endl;
+    for (int i = 0; i < row; i++)
     {
-        for(int j=0; j<col;j++)
+        for (int j = 0; j < col; j++)
         {
-            cin>>arr2[i][j];
+            cin >> arr2[i][j];
         }
     }
     // Sum
-    for(int i=0;i<row;i++)
+    for (int i = 0; i < row; i++)
     {
-        for(int j=0; j<col;j++)
+        for (int j = 0; j < col; j++)
         {
-            cout<<arr[i][j]+arr2[i][j]<<" ";
+            cout << arr[i][j] + arr2[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
